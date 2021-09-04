39992bc048e2a2fbd5569731c39a3780fb246ea4
LEv145 <40071054+LEv145@users.noreply.github.com>
2021-09-04 19:42:37 +0200

    Обновил описание
    Советаю поставить pydocstyle

--- tests/test_disio.py
+++ tests/test_disio.py
@@ -3,8 +3,8 @@ import unittest
 from disio import __version__
 
 
-class Toaster(unittest.TestCase):
-    """Useless tests for disio."""
-
+class Toster(unittest.TestCase):
+    """Class for tests"""
     def test_version(self):
+        """Checks the bot version for equivalence"""
         self.assertEqual(__version__, "0.1.0")
